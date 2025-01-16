1f1f0efe710610f704c3051c64b43d2d9ce69542
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2025-01-16 08:46:32 +0100

    Fixing for deployment

--- main.py
+++ main.py
@@ -190,5 +190,8 @@ def run_model():
 
     return jsonify(results)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.getenv("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
